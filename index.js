6f6d3a12d3691cc132b2f3dc74c5e8bacb3570f8
mohiandipta <mohianmostofa@gmail.com>
2021-11-01 11:57:54 +0600

    mongodb connected to the server successfully

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-app.get(`/api`, (req, res) => {
-    res.send('Hello Node.js')
+const morgan = require('morgan')
+require('dotenv').config()
+
+//db connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true
+}).then(() => console.log('db connected'))
+    .catch((err) => console.log('db connection error =>', err))
+
+app.get(`/user`, (req, res) => {
+    let user = {
+        fName: 'mohian',
+        lName: 'dipta',
+        email: 'mohian@gmail.com',
+        password: '123456'
+    }
+    res.send(user)
 })
 
-app.listen('3000', (req, res) => {
-    console.log('server is live on localhost:3000')
+app.listen('8000', (req, res) => {
+    console.log('server is live on localhost:8000')
 })
