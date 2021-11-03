29b7af9500dae7b1ff77175ce44b62a4f83a075e
mohiandipta <mohianmostofa@gmail.com>
2021-11-03 12:44:25 +0600

    register user data succesfully posted to the mongodb

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt'
 
 export const hashPassword = (password) => {
-    return new Promis((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         bcrypt.genSalt(12, (err, salt) => {
             if (err) {
                 reject(err)
